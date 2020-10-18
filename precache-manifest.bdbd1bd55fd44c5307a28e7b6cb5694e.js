833316f461f83e4b46e969a2d5507328696f52a3
Joey Ramos <joey@Joeys-Mac-mini.local>
2020-10-17 20:31:02 -0500

    Updates

---
+++ precache-manifest.bdbd1bd55fd44c5307a28e7b6cb5694e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa51bdb35ae9898fe698749768f3859a",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "48601417faa64fbab0a8",
+    "url": "/react-portfolio/static/css/main.13ce05ab.chunk.css"
+  },
+  {
+    "revision": "1a6223530ac494c7c37e",
+    "url": "/react-portfolio/static/js/2.42845391.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.42845391.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48601417faa64fbab0a8",
+    "url": "/react-portfolio/static/js/main.28fc3e3f.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
